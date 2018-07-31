1a279b84769d3681095b92bcee63150f81e61c87
KovalenkoAV <merlinwt@gmail.com>
2018-07-31 21:11:13 +0300

    Add include base mvc classes
    ...
    
    Adding into http enter point including of Controller, Model and View classes.
    
    mvc:base

--- http.php
+++ http.php
@@ -1,5 +1,9 @@
 <?php
 
-	$controller = new Controller();
+	include_once 'Controller.php';
+	include_once 'Model.php';
+	include_once 'View.php';
+
+	$controller = new \Controller();
 	
 	$controller->indexAction();
