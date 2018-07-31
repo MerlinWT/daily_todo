b286f85e8292128bda9cb21083c21dbfa5302b57
KovalenkoAV <merlinwt@gmail.com>
2018-07-31 21:35:56 +0300

    Add render days template
    ...
    
    Adding into View method to render template of days
    
    body:days

--- View.php
+++ View.php
@@ -11,8 +11,45 @@ class View
 	/**
 	 * @param array $data
 	 */
-	public function render($data) {
-		extract($data);
-		include __DIR__ . '/html/layout.html';
+	protected function layout($data) {
+		try {
+			echo $this->templateHtml('layout', $data);
+		} catch (Exception $e) {
+			echo 'Layout render error: ' . $e->getMessage();
+		}
 	}
+	
+	public function days($data) {
+		extract($data);
+		
+		try {
+			$body = $this->templateHtml('days', [
+				'days' => '1,2,3,4',
+				'content' => 'First, select day'
+			]);
+		} catch (Exception $e) {
+			$body = 'Days render error: ' . $e->getMessage();
+		}
+		
+		$this->layout([
+			'body' => $body
+		]);
+	}
+	
+	/**
+	 * @param string $name
+	 * @param array $data
+	 * @return string
+	 * @throws Exception
+	 */
+	protected function templateHtml($name, $data) {
+		if (array_key_exists('file', $data)) throw new \Exception('key `file` not allowed into template data');
+		extract($data);
+		$file = "/html/{$name}.html";
+		
+		ob_start();
+		include __DIR__ . $file;
+		return ob_get_flush();
+	}
+	
 }
